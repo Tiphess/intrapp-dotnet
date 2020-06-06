d764757a4c6d5a91598152c074634f1b1fae40c4
guillaume cadieux <guillaume.cadieux@lepisc.com>
2020-06-06 15:28:45 -0400

    Initial commit

---
+++ Models/Summoner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace intrapp.Models
+{
+    public class Summoner
+    {
+        public string Json { get; set; }
+    }
+}
